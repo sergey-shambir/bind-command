d46d0a14806286e6fd04e23d01b66c5e1048ac0b
Sergey Shambir <sshambir@yandex.ru>
2017-08-29 08:03:22 +0300

    Добавил интерактивную оболоку для тестирования команд

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,73 @@
 #include "BindCommand.h"
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
 #include <iostream>
+#include <map>
 
-using namespace std::literals;
+using namespace std;
+
+using Command = function<string(const vector<string>&)>;
+
+map<string, Command> BuildCommandMap()
+{
+	return {
+		{ "hello", BindCommand([](const std::string& name) {
+			 std::cout << "Hello, " << name << std::endl;
+		 }) },
+		{ "sum", BindCommand([](int a, int b) {
+			 return a + b;
+		 }) },
+		{ "exit", BindCommand([]() {
+			 exit(0);
+		 }) },
+	};
+}
+
+pair<string, vector<string>> SplitCommandAndArgs(const string& str)
+{
+	string commandName;
+	vector<string> arguments;
+
+	boost::split(arguments, str, boost::is_space(), boost::token_compress_on);
+	if (!arguments.empty())
+	{
+		commandName = arguments.front();
+		arguments.erase(arguments.begin());
+	}
+
+	return { commandName, arguments };
+}
 
 int main()
 {
-	auto command = BindCommand([](const std::string& name) {
-		std::cout << "Hello, " << name << std::endl;
-	});
+	auto commands = BuildCommandMap();
 
-	command(std::vector{ "Vasya"s });
+	std::string input;
+	while (std::getline(std::cin, input))
+	{
+		auto[commandName, args] = SplitCommandAndArgs(input);
+		if (commandName.empty())
+		{
+			continue;
+		}
+		auto commandIt = commands.find(commandName);
+		if (commandIt == commands.end())
+		{
+			cerr << "invalid command: " << commandName << endl;
+			continue;
+		}
 
-	auto command2 = BindCommand([](int a, int b) {
-		return a + b;
-	});
-
-	std::string result = command2(std::vector{ "1941"s, "4"s });
-	std::cout << result << std::endl;
+		try
+		{
+			const string result = commandIt->second(args);
+			if (!result.empty())
+			{
+				cout << "result: " << result << endl;
+			}
+		}
+		catch (const std::exception& ex)
+		{
+			cerr << "Call failed: " << ex.what() << endl;
+		}
+	}
 }
